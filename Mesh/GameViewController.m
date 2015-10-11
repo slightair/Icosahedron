e8c472839f59c855f07e8200dad2e8af81cb3c95
slightair <arksutite@gmail.com>
2015-10-11 12:44:21 +0900

    Draw Archimedes' Spiral

--- Mesh/GameViewController.m
+++ Mesh/GameViewController.m
@@ -1,19 +1,11 @@
 #import "GameViewController.h"
 @import OpenGLES;
+@import UIKit;
 
 #define BUFFER_OFFSET(i) ((char *)NULL + (i))
 
-const GLfloat vertices[] = {
-     0.5, -0.5, 0.0,  0.0, 0.0, 1.0, 1.0,
-    -0.5, -0.5, 0.0,  1.0, 0.0, 0.0, 1.0,
-    -0.5,  0.5, 0.0,  0.0, 1.0, 0.0, 1.0,
-     0.5,  0.5, 0.0,  1.0, 1.0, 0.0, 1.0,
-};
-
-const GLubyte indices[] = {
-    0, 1, 2,
-    0, 3, 2,
-};
+const NSInteger NUM_VERTICES = 128;
+const NSInteger NUM_SPLIT = 24;
 
 @interface GameViewController ()
 
@@ -26,6 +18,9 @@ const GLubyte indices[] = {
 {
     GLuint vertexBufferID;
     GLuint indexBufferID;
+    GLfloat *vertices;
+    GLubyte *indices;
+    int indicesCount;
 }
 
 - (void)dealloc
@@ -38,6 +33,9 @@ const GLubyte indices[] = {
     self.context = nil;
 
     [EAGLContext setCurrentContext:nil];
+
+    free(vertices);
+    free(indices);
 }
 
 - (void)viewDidLoad
@@ -56,6 +54,52 @@ const GLubyte indices[] = {
     self.effect.constantColor = GLKVector4Make(1.0, 1.0, 1.0, 1.0);
 
     glClearColor(0.0, 0.0, 0.0, 1.0);
+
+    [self createVertices];
+}
+
+- (void)createVertices
+{
+    vertices = malloc(sizeof(GLfloat) * 7 * NUM_VERTICES);
+    indices = malloc(sizeof(GLubyte) * (NUM_SPLIT * 2 + NUM_VERTICES * 4));
+    indicesCount = 0;
+
+    vertices[0] = 0.0; vertices[1] = 0.0; vertices[2] = 0.0;
+    vertices[3] = 1.0; vertices[4] = 1.0; vertices[5] = 1.0; vertices[6] = 1.0;
+
+    for (int i = 1; i <= NUM_SPLIT; i++) {
+        indices[indicesCount++] = 0;
+        indices[indicesCount++] = i;
+    }
+
+    for (int i = 1; i < NUM_VERTICES; i++) {
+        int base;
+        double theta = 2 * M_PI / NUM_SPLIT * i;
+        double radius = 0.05 * theta;
+
+        base = i * 7;
+        vertices[base + 0] = radius * cos(theta);
+        vertices[base + 1] = radius * sin(theta);
+        vertices[base + 2] = 0.0;
+
+        UIColor *color = [UIColor colorWithHue:fmod(theta, (2 * M_PI)) / (2 * M_PI) saturation:0.5 brightness:1.0 alpha:1.0];
+        CGFloat red, green, blue;
+        [color getRed:&red green:&green blue:&blue alpha:NULL];
+        vertices[base + 3] = (GLfloat)red;
+        vertices[base + 4] = (GLfloat)green;
+        vertices[base + 5] = (GLfloat)blue;
+        vertices[base + 6] = 1.0;
+
+        if (i + 1 < NUM_VERTICES) {
+            indices[indicesCount++] = i;
+            indices[indicesCount++] = i + 1;
+        }
+
+        if (i + NUM_SPLIT < NUM_VERTICES) {
+            indices[indicesCount++] = i;
+            indices[indicesCount++] = i + NUM_SPLIT;
+        }
+    }
 }
 
 #pragma mark - GLKViewDelegate methods
@@ -68,11 +112,11 @@ const GLubyte indices[] = {
 
     glGenBuffers(1, &vertexBufferID);
     glBindBuffer(GL_ARRAY_BUFFER, vertexBufferID);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat) * 7 * NUM_VERTICES, vertices, GL_STATIC_DRAW);
 
     glGenBuffers(1, &indexBufferID);
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBufferID);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(GLubyte) * indicesCount, indices, GL_STATIC_DRAW);
 
     glEnableVertexAttribArray(GLKVertexAttribPosition);
     glEnableVertexAttribArray(GLKVertexAttribColor);
@@ -81,7 +125,7 @@ const GLubyte indices[] = {
     glVertexAttribPointer(GLKVertexAttribPosition, 3, GL_FLOAT, GL_FALSE, stride, BUFFER_OFFSET(0));
     glVertexAttribPointer(GLKVertexAttribColor, 4, GL_FLOAT, GL_FALSE, stride, BUFFER_OFFSET(sizeof(GLfloat) * 3));
 
-    glDrawElements(GL_LINE_LOOP, sizeof(indices) / sizeof(GLubyte), GL_UNSIGNED_BYTE, NULL);
+    glDrawElements(GL_LINE_STRIP, indicesCount, GL_UNSIGNED_BYTE, NULL);
 }
 
 @end
