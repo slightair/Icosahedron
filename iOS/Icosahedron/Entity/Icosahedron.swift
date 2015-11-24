54b1b6105b182de7504ef9ed83ec5bc8bee5c08f
slightair <arksutite@gmail.com>
2015-11-24 22:45:42 +0900

    Introduce Icosahedron.Side

--- iOS/Icosahedron/Entity/Icosahedron.swift
+++ iOS/Icosahedron/Entity/Icosahedron.swift
@@ -19,4 +19,41 @@ class Icosahedron {
             return [A, B, C, D, E, F, G, H, I, J, K, L]
         }
     }
+
+    enum Side: String {
+        case AB = "AB"
+        case AC = "AC"
+        case AE = "AE"
+        case AF = "AF"
+        case AG = "AG"
+        case BC = "BC"
+        case BD = "BD"
+        case BF = "BF"
+        case BH = "BH"
+        case CD = "CD"
+        case CE = "CE"
+        case CI = "CI"
+        case DH = "DH"
+        case DI = "DI"
+        case DJ = "DJ"
+        case EI = "EI"
+        case EK = "EK"
+        case FG = "FG"
+        case FH = "FH"
+        case FL = "FL"
+        case GE = "GE"
+        case GK = "GK"
+        case GL = "GL"
+        case HJ = "HJ"
+        case HL = "HL"
+        case IJ = "IJ"
+        case IK = "IK"
+        case JK = "JK"
+        case JL = "JL"
+        case KL = "KL"
+
+        static var values: [Side] {
+            return [AB, AC, AE, AF, AG, BC, BD, BF, BH, CD, CE, CI, DH, DI, DJ, EI, EK, FG, FH, FL, GE, GK, GL, HJ, HL, IJ, IK, JK, JL, KL]
+        }
+    }
 }
--- iOS/Icosahedron/Entity/Road.swift
+++ iOS/Icosahedron/Entity/Road.swift
@@ -3,4 +3,9 @@ import Foundation
 struct Road {
     let left: Icosahedron.Point
     let right: Icosahedron.Point
+
+    init(side: Icosahedron.Side) {
+        left = Icosahedron.Point(rawValue: side.rawValue.substringToIndex(side.rawValue.startIndex.advancedBy(1)))!
+        right = Icosahedron.Point(rawValue: side.rawValue.substringFromIndex(side.rawValue.startIndex.advancedBy(1)))!
+    }
 }
