0603fb49f0651101801bb43789260c57034cc9ca
slightair <arksutite@gmail.com>
2016-02-09 22:50:09 +0900

    Split Renderer class

---
+++ iOS/Icosahedron/Scene/BaseRenderer.swift
@@ -0,0 +1,162 @@
+import GLKit
+
+class BaseRenderer {
+    var modelVertexArray: GLuint = 0
+    var modelVertexBuffer: GLuint = 0
+    var modelIndexBuffer: GLuint = 0
+
+    var modelFrameBuffer: GLuint = 0
+    var modelColorTexture: GLuint = 0
+    var modelDepthRenderBuffer: GLuint = 0
+
+    var modelProjectionMatrix = GLKMatrix4Identity
+    var worldMatrix = GLKMatrix4Identity
+    var normalMatrix = GLKMatrix3Identity
+
+    var backgroundProjectionMatrix = GLKMatrix4Identity
+    var backgroundWorldMatrix = GLKMatrix4Identity
+    var backgroundNormalMatrix = GLKMatrix3Identity
+
+    deinit {
+        tearDownGL()
+    }
+
+    init() {
+        setUp()
+    }
+
+    func setUpGL() {
+        let projectionWidth: Float = 1.0
+        let projectionHeight = projectionWidth / Screen.aspect
+        modelProjectionMatrix = GLKMatrix4MakeOrtho(-projectionWidth / 2, projectionWidth / 2, -projectionHeight / 2, projectionHeight / 2, 0.1, 10)
+        backgroundProjectionMatrix = GLKMatrix4MakePerspective(120, Screen.aspect, 0.1, 10)
+
+        glGenVertexArrays(1, &modelVertexArray)
+        glBindVertexArray(modelVertexArray)
+
+        var buffers: [GLuint] = [GLuint](count: 2, repeatedValue: 0)
+        glGenBuffers(2, &buffers)
+        modelVertexBuffer = buffers[0]
+        modelIndexBuffer = buffers[1]
+
+        glBindVertexArray(0)
+
+        let width = GLsizei(CGRectGetHeight(UIScreen.mainScreen().nativeBounds)) // long side
+        let height = GLsizei(CGRectGetWidth(UIScreen.mainScreen().nativeBounds)) // short side
+
+        glGenTextures(1, &modelColorTexture)
+        glBindTexture(GLenum(GL_TEXTURE_2D), modelColorTexture)
+        glTexImage2D(GLenum(GL_TEXTURE_2D), 0, GLint(GL_RGBA), width, height, 0, GLenum(GL_RGBA), GLenum(GL_UNSIGNED_BYTE), nil)
+        glTexParameteri(GLenum(GL_TEXTURE_2D), GLenum(GL_TEXTURE_MAG_FILTER), GLint(GL_LINEAR))
+        glTexParameteri(GLenum(GL_TEXTURE_2D), GLenum(GL_TEXTURE_MIN_FILTER), GLint(GL_LINEAR))
+        glTexParameteri(GLenum(GL_TEXTURE_2D), GLenum(GL_TEXTURE_WRAP_S), GLint(GL_CLAMP_TO_EDGE))
+        glTexParameteri(GLenum(GL_TEXTURE_2D), GLenum(GL_TEXTURE_WRAP_T), GLint(GL_CLAMP_TO_EDGE))
+        glBindTexture(GLenum(GL_TEXTURE_2D), 0)
+
+        glGenRenderbuffers(1, &modelDepthRenderBuffer)
+        glBindRenderbuffer(GLenum(GL_RENDERBUFFER), modelDepthRenderBuffer)
+        glRenderbufferStorage(GLenum(GL_RENDERBUFFER), GLenum(GL_DEPTH_COMPONENT24), width, height)
+        glBindRenderbuffer(GLenum(GL_RENDERBUFFER), 0)
+
+        glGenFramebuffers(1, &modelFrameBuffer)
+        glBindFramebuffer(GLenum(GL_FRAMEBUFFER), modelFrameBuffer)
+        glFramebufferTexture2D(GLenum(GL_FRAMEBUFFER), GLenum(GL_COLOR_ATTACHMENT0), GLenum(GL_TEXTURE_2D), modelColorTexture, 0)
+        glFramebufferRenderbuffer(GLenum(GL_FRAMEBUFFER), GLenum(GL_DEPTH_ATTACHMENT), GLenum(GL_RENDERBUFFER), modelDepthRenderBuffer)
+        guard glCheckFramebufferStatus(GLenum(GL_FRAMEBUFFER)) == GLenum(GL_FRAMEBUFFER_COMPLETE) else {
+            fatalError("Check frame buffer status error!")
+        }
+        glBindFramebuffer(GLenum(GL_FRAMEBUFFER), 0)
+    }
+
+    func tearDownGL() {
+        glDeleteBuffers(1, &modelVertexBuffer)
+        glDeleteBuffers(1, &modelIndexBuffer)
+        glDeleteVertexArrays(1, &modelVertexArray)
+
+        glDeleteTextures(1, &modelColorTexture)
+        glDeleteRenderbuffers(1, &modelDepthRenderBuffer)
+        glDeleteFramebuffers(1, &modelFrameBuffer)
+    }
+
+    func setUp() {
+        setUpGL()
+    }
+
+    func drawPolygons(polygons: [RenderablePolygon]) {
+        let modelVertices = polygons.flatMap { $0.modelVertices }
+        let vertices: [Float] = modelVertices.flatMap { $0.v }
+        let indexes: [GLushort] = polygons.flatMap { $0.modelIndexes }
+
+        glBindVertexArray(modelVertexArray)
+
+        glBindBuffer(GLenum(GL_ARRAY_BUFFER), modelVertexBuffer)
+        glBufferData(GLenum(GL_ARRAY_BUFFER), GLsizeiptr(ModelVertex.size * modelVertices.count), vertices, GLenum(GL_STREAM_DRAW))
+
+        glEnableVertexAttribArray(GLuint(GLKVertexAttrib.Position.rawValue))
+        glVertexAttribPointer(GLuint(GLKVertexAttrib.Position.rawValue), 3, GLenum(GL_FLOAT), GLboolean(GL_FALSE), GLsizei(ModelVertex.size), BUFFER_OFFSET(0))
+
+        glEnableVertexAttribArray(GLuint(GLKVertexAttrib.Normal.rawValue))
+        glVertexAttribPointer(GLuint(GLKVertexAttrib.Normal.rawValue), 3, GLenum(GL_FLOAT), GLboolean(GL_FALSE), GLsizei(ModelVertex.size), BUFFER_OFFSET(sizeof(Float) * 3))
+
+        glEnableVertexAttribArray(GLuint(GLKVertexAttrib.Color.rawValue))
+        glVertexAttribPointer(GLuint(GLKVertexAttrib.Color.rawValue), 4, GLenum(GL_FLOAT), GLboolean(GL_FALSE), GLsizei(ModelVertex.size), BUFFER_OFFSET(sizeof(Float) * 6))
+
+        glEnableVertexAttribArray(GLuint(GLKVertexAttrib.TexCoord0.rawValue))
+        glVertexAttribPointer(GLuint(GLKVertexAttrib.TexCoord0.rawValue), 2, GLenum(GL_FLOAT), GLboolean(GL_FALSE), GLsizei(ModelVertex.size), BUFFER_OFFSET(sizeof(Float) * 10))
+
+        glBindBuffer(GLenum(GL_ELEMENT_ARRAY_BUFFER), modelIndexBuffer)
+        glBufferData(GLenum(GL_ELEMENT_ARRAY_BUFFER), GLsizeiptr(sizeof(GLushort) * indexes.count), indexes, GLenum(GL_STREAM_DRAW))
+
+        glDrawElements(GLenum(GL_TRIANGLE_STRIP), GLsizei(indexes.count), GLenum(GL_UNSIGNED_SHORT), nil)
+
+        glBindVertexArray(0)
+    }
+
+    func drawModels(models: [Renderable]) {
+        let modelVertices = models.flatMap { $0.modelVertices }
+        let vertices: [Float] = modelVertices.flatMap { $0.v }
+
+        glBindVertexArray(modelVertexArray)
+        glBindBuffer(GLenum(GL_ARRAY_BUFFER), modelVertexBuffer)
+
+        glBufferData(GLenum(GL_ARRAY_BUFFER), GLsizeiptr(ModelVertex.size * modelVertices.count), vertices, GLenum(GL_STREAM_DRAW))
+
+        glEnableVertexAttribArray(GLuint(GLKVertexAttrib.Position.rawValue))
+        glVertexAttribPointer(GLuint(GLKVertexAttrib.Position.rawValue), 3, GLenum(GL_FLOAT), GLboolean(GL_FALSE), GLsizei(ModelVertex.size), BUFFER_OFFSET(0))
+
+        glEnableVertexAttribArray(GLuint(GLKVertexAttrib.Normal.rawValue))
+        glVertexAttribPointer(GLuint(GLKVertexAttrib.Normal.rawValue), 3, GLenum(GL_FLOAT), GLboolean(GL_FALSE), GLsizei(ModelVertex.size), BUFFER_OFFSET(sizeof(Float) * 3))
+
+        glEnableVertexAttribArray(GLuint(GLKVertexAttrib.Color.rawValue))
+        glVertexAttribPointer(GLuint(GLKVertexAttrib.Color.rawValue), 4, GLenum(GL_FLOAT), GLboolean(GL_FALSE), GLsizei(ModelVertex.size), BUFFER_OFFSET(sizeof(Float) * 6))
+
+        glEnableVertexAttribArray(GLuint(GLKVertexAttrib.TexCoord0.rawValue))
+        glVertexAttribPointer(GLuint(GLKVertexAttrib.TexCoord0.rawValue), 2, GLenum(GL_FLOAT), GLboolean(GL_FALSE), GLsizei(ModelVertex.size), BUFFER_OFFSET(sizeof(Float) * 10))
+
+        glDrawArrays(GLenum(GL_TRIANGLES), 0, GLsizei(modelVertices.count))
+
+        glBindVertexArray(0)
+    }
+
+    func drawParticle(points: [ParticleVertex]) {
+        let vertices: [Float] = points.flatMap { $0.v }
+
+        glBindVertexArray(modelVertexArray)
+        glBindBuffer(GLenum(GL_ARRAY_BUFFER), modelVertexBuffer)
+
+        glBufferData(GLenum(GL_ARRAY_BUFFER), GLsizeiptr(ParticleVertex.size * points.count), vertices, GLenum(GL_STREAM_DRAW))
+
+        glEnableVertexAttribArray(GLuint(GLKVertexAttrib.Position.rawValue))
+        glVertexAttribPointer(GLuint(GLKVertexAttrib.Position.rawValue), 3, GLenum(GL_FLOAT), GLboolean(GL_FALSE), GLsizei(ParticleVertex.size), BUFFER_OFFSET(0))
+
+        glEnableVertexAttribArray(GLuint(GLKVertexAttrib.Color.rawValue))
+        glVertexAttribPointer(GLuint(GLKVertexAttrib.Color.rawValue), 4, GLenum(GL_FLOAT), GLboolean(GL_FALSE), GLsizei(ParticleVertex.size), BUFFER_OFFSET(sizeof(Float) * 3))
+
+        glEnableVertexAttribArray(ParticleShaderProgram.PointSizeAttribLocation)
+        glVertexAttribPointer(ParticleShaderProgram.PointSizeAttribLocation, 1, GLenum(GL_FLOAT), GLboolean(GL_FALSE), GLsizei(ParticleVertex.size), BUFFER_OFFSET(sizeof(Float) * 7))
+
+        glDrawArrays(GLenum(GL_POINTS), 0, GLsizei(points.count))
+
+        glBindVertexArray(0)
+    }
+}
--- iOS/Icosahedron/Scene/GameSceneRenderer.swift
+++ iOS/Icosahedron/Scene/GameSceneRenderer.swift
@@ -1,34 +1,16 @@
 import GLKit
-import OpenGLES
 
 protocol GameSceneRendererDelegate {
     func didChangeIcosahedronPoint(point: Icosahedron.Point)
 }
 
-class GameSceneRenderer {
-    var modelVertexArray: GLuint = 0
-    var modelVertexBuffer: GLuint = 0
-    var modelIndexBuffer: GLuint = 0
-
-    var modelFrameBuffer: GLuint = 0
-    var modelColorTexture: GLuint = 0
-    var modelDepthRenderBuffer: GLuint = 0
-
+class GameSceneRenderer: BaseRenderer {
     var backgroundShaderProgram: BackgroundShaderProgram!
     var modelShaderProgram: ModelShaderProgram!
     var particleShaderProgram: ParticleShaderProgram!
     var canvasShaderProgram: CanvasShaderProgram!
     var uiShaderProgram: UIShaderProgram!
 
-    var backgroundProjectionMatrix = GLKMatrix4Identity
-    var backgroundWorldMatrix = GLKMatrix4Identity
-    var backgroundNormalMatrix = GLKMatrix3Identity
-
-    var modelProjectionMatrix = GLKMatrix4Identity
-    var worldMatrix = GLKMatrix4Identity
-    var normalMatrix = GLKMatrix3Identity
-
-    let fontData: FontData = FontData.defaultData
     var meshTextureInfo: GLKTextureInfo!
     var whiteTextureInfo: GLKTextureInfo!
     var pointTextureInfo: GLKTextureInfo!
@@ -57,45 +39,28 @@ class GameSceneRenderer {
         }
     }
 
-    deinit {
-        tearDownGL()
-    }
-
     init(world: World) {
         self.world = world
         self.modelProducer = GameSceneModelProducer(world: world)
+
+        super.init()
 
         currentVertex = modelProducer.icosahedronModel.pointDict[.C]
 
         let dummyVertex = modelProducer.icosahedronModel.pointDict[.F]!
         modelProducer.markerModel.setPosition(currentVertex.coordinate, prevPosition: dummyVertex.coordinate)
 
-        setUpGL()
-
         update()
     }
 
-    private func setUpGL() {
+    override func setUp() {
+        super.setUp()
+
         backgroundShaderProgram = BackgroundShaderProgram()
         modelShaderProgram = ModelShaderProgram()
         particleShaderProgram = ParticleShaderProgram()
         canvasShaderProgram = CanvasShaderProgram()
         uiShaderProgram = UIShaderProgram()
-
-        let projectionWidth: Float = 1.0
-        let projectionHeight = projectionWidth / Screen.aspect
-        modelProjectionMatrix = GLKMatrix4MakeOrtho(-projectionWidth / 2, projectionWidth / 2, -projectionHeight / 2, projectionHeight / 2, 0.1, 10)
-        backgroundProjectionMatrix = GLKMatrix4MakePerspective(120, Screen.aspect, 0.1, 10)
-
-        glGenVertexArrays(1, &modelVertexArray)
-        glBindVertexArray(modelVertexArray)
-
-        var buffers: [GLuint] = [GLuint](count: 2, repeatedValue: 0)
-        glGenBuffers(2, &buffers)
-        modelVertexBuffer = buffers[0]
-        modelIndexBuffer = buffers[1]
-
-        glBindVertexArray(0)
 
         guard let meshTextureAsset = NSDataAsset(name: "Mesh") else {
             fatalError("debug texture file not found")
@@ -112,43 +77,7 @@ class GameSceneRenderer {
         }
         pointTextureInfo = try! GLKTextureLoader.textureWithContentsOfData(pointTextureAsset.data, options: nil)
 
-        fontData.loadTexture()
-
-        let width = GLsizei(CGRectGetHeight(UIScreen.mainScreen().nativeBounds)) // long side
-        let height = GLsizei(CGRectGetWidth(UIScreen.mainScreen().nativeBounds)) // short side
-
-        glGenTextures(1, &modelColorTexture)
-        glBindTexture(GLenum(GL_TEXTURE_2D), modelColorTexture)
-        glTexImage2D(GLenum(GL_TEXTURE_2D), 0, GLint(GL_RGBA), width, height, 0, GLenum(GL_RGBA), GLenum(GL_UNSIGNED_BYTE), nil)
-        glTexParameteri(GLenum(GL_TEXTURE_2D), GLenum(GL_TEXTURE_MAG_FILTER), GLint(GL_LINEAR))
-        glTexParameteri(GLenum(GL_TEXTURE_2D), GLenum(GL_TEXTURE_MIN_FILTER), GLint(GL_LINEAR))
-        glTexParameteri(GLenum(GL_TEXTURE_2D), GLenum(GL_TEXTURE_WRAP_S), GLint(GL_CLAMP_TO_EDGE))
-        glTexParameteri(GLenum(GL_TEXTURE_2D), GLenum(GL_TEXTURE_WRAP_T), GLint(GL_CLAMP_TO_EDGE))
-        glBindTexture(GLenum(GL_TEXTURE_2D), 0)
-
-        glGenRenderbuffers(1, &modelDepthRenderBuffer)
-        glBindRenderbuffer(GLenum(GL_RENDERBUFFER), modelDepthRenderBuffer)
-        glRenderbufferStorage(GLenum(GL_RENDERBUFFER), GLenum(GL_DEPTH_COMPONENT24), width, height)
-        glBindRenderbuffer(GLenum(GL_RENDERBUFFER), 0)
-
-        glGenFramebuffers(1, &modelFrameBuffer)
-        glBindFramebuffer(GLenum(GL_FRAMEBUFFER), modelFrameBuffer)
-        glFramebufferTexture2D(GLenum(GL_FRAMEBUFFER), GLenum(GL_COLOR_ATTACHMENT0), GLenum(GL_TEXTURE_2D), modelColorTexture, 0)
-        glFramebufferRenderbuffer(GLenum(GL_FRAMEBUFFER), GLenum(GL_DEPTH_ATTACHMENT), GLenum(GL_RENDERBUFFER), modelDepthRenderBuffer)
-        guard glCheckFramebufferStatus(GLenum(GL_FRAMEBUFFER)) == GLenum(GL_FRAMEBUFFER_COMPLETE) else {
-            fatalError("Check frame buffer status error!")
-        }
-        glBindFramebuffer(GLenum(GL_FRAMEBUFFER), 0)
-    }
-
-    private func tearDownGL() {
-        glDeleteBuffers(1, &modelVertexBuffer)
-        glDeleteBuffers(1, &modelIndexBuffer)
-        glDeleteVertexArrays(1, &modelVertexArray)
-
-        glDeleteTextures(1, &modelColorTexture)
-        glDeleteRenderbuffers(1, &modelDepthRenderBuffer)
-        glDeleteFramebuffers(1, &modelFrameBuffer)
+        FontData.defaultData.loadTexture()
     }
 
     private func moveToVertex(vertex: IcosahedronVertex) {
@@ -160,84 +89,6 @@ class GameSceneRenderer {
 
         prevQuaternion = currentQuaternion
         currentQuaternion = GLKQuaternionMultiply(currentQuaternion, relativeQuaternion)
-    }
-
-    private func drawPolygons(polygons: [RenderablePolygon]) {
-        let modelVertices = polygons.flatMap { $0.modelVertices }
-        let vertices: [Float] = modelVertices.flatMap { $0.v }
-        let indexes: [GLushort] = polygons.flatMap { $0.modelIndexes }
-
-        glBindVertexArray(modelVertexArray)
-
-        glBindBuffer(GLenum(GL_ARRAY_BUFFER), modelVertexBuffer)
-        glBufferData(GLenum(GL_ARRAY_BUFFER), GLsizeiptr(ModelVertex.size * modelVertices.count), vertices, GLenum(GL_STREAM_DRAW))
-
-        glEnableVertexAttribArray(GLuint(GLKVertexAttrib.Position.rawValue))
-        glVertexAttribPointer(GLuint(GLKVertexAttrib.Position.rawValue), 3, GLenum(GL_FLOAT), GLboolean(GL_FALSE), GLsizei(ModelVertex.size), BUFFER_OFFSET(0))
-
-        glEnableVertexAttribArray(GLuint(GLKVertexAttrib.Normal.rawValue))
-        glVertexAttribPointer(GLuint(GLKVertexAttrib.Normal.rawValue), 3, GLenum(GL_FLOAT), GLboolean(GL_FALSE), GLsizei(ModelVertex.size), BUFFER_OFFSET(sizeof(Float) * 3))
-
-        glEnableVertexAttribArray(GLuint(GLKVertexAttrib.Color.rawValue))
-        glVertexAttribPointer(GLuint(GLKVertexAttrib.Color.rawValue), 4, GLenum(GL_FLOAT), GLboolean(GL_FALSE), GLsizei(ModelVertex.size), BUFFER_OFFSET(sizeof(Float) * 6))
-
-        glEnableVertexAttribArray(GLuint(GLKVertexAttrib.TexCoord0.rawValue))
-        glVertexAttribPointer(GLuint(GLKVertexAttrib.TexCoord0.rawValue), 2, GLenum(GL_FLOAT), GLboolean(GL_FALSE), GLsizei(ModelVertex.size), BUFFER_OFFSET(sizeof(Float) * 10))
-
-        glBindBuffer(GLenum(GL_ELEMENT_ARRAY_BUFFER), modelIndexBuffer)
-        glBufferData(GLenum(GL_ELEMENT_ARRAY_BUFFER), GLsizeiptr(sizeof(GLushort) * indexes.count), indexes, GLenum(GL_STREAM_DRAW))
-
-        glDrawElements(GLenum(GL_TRIANGLE_STRIP), GLsizei(indexes.count), GLenum(GL_UNSIGNED_SHORT), nil)
-
-        glBindVertexArray(0)
-    }
-
-    private func drawModels(models: [Renderable]) {
-        let modelVertices = models.flatMap { $0.modelVertices }
-        let vertices: [Float] = modelVertices.flatMap { $0.v }
-
-        glBindVertexArray(modelVertexArray)
-        glBindBuffer(GLenum(GL_ARRAY_BUFFER), modelVertexBuffer)
-
-        glBufferData(GLenum(GL_ARRAY_BUFFER), GLsizeiptr(ModelVertex.size * modelVertices.count), vertices, GLenum(GL_STREAM_DRAW))
-
-        glEnableVertexAttribArray(GLuint(GLKVertexAttrib.Position.rawValue))
-        glVertexAttribPointer(GLuint(GLKVertexAttrib.Position.rawValue), 3, GLenum(GL_FLOAT), GLboolean(GL_FALSE), GLsizei(ModelVertex.size), BUFFER_OFFSET(0))
-
-        glEnableVertexAttribArray(GLuint(GLKVertexAttrib.Normal.rawValue))
-        glVertexAttribPointer(GLuint(GLKVertexAttrib.Normal.rawValue), 3, GLenum(GL_FLOAT), GLboolean(GL_FALSE), GLsizei(ModelVertex.size), BUFFER_OFFSET(sizeof(Float) * 3))
-
-        glEnableVertexAttribArray(GLuint(GLKVertexAttrib.Color.rawValue))
-        glVertexAttribPointer(GLuint(GLKVertexAttrib.Color.rawValue), 4, GLenum(GL_FLOAT), GLboolean(GL_FALSE), GLsizei(ModelVertex.size), BUFFER_OFFSET(sizeof(Float) * 6))
-
-        glEnableVertexAttribArray(GLuint(GLKVertexAttrib.TexCoord0.rawValue))
-        glVertexAttribPointer(GLuint(GLKVertexAttrib.TexCoord0.rawValue), 2, GLenum(GL_FLOAT), GLboolean(GL_FALSE), GLsizei(ModelVertex.size), BUFFER_OFFSET(sizeof(Float) * 10))
-
-        glDrawArrays(GLenum(GL_TRIANGLES), 0, GLsizei(modelVertices.count))
-
-        glBindVertexArray(0)
-    }
-
-    private func drawParticle(points: [ParticleVertex]) {
-        let vertices: [Float] = points.flatMap { $0.v }
-
-        glBindVertexArray(modelVertexArray)
-        glBindBuffer(GLenum(GL_ARRAY_BUFFER), modelVertexBuffer)
-
-        glBufferData(GLenum(GL_ARRAY_BUFFER), GLsizeiptr(ParticleVertex.size * points.count), vertices, GLenum(GL_STREAM_DRAW))
-
-        glEnableVertexAttribArray(GLuint(GLKVertexAttrib.Position.rawValue))
-        glVertexAttribPointer(GLuint(GLKVertexAttrib.Position.rawValue), 3, GLenum(GL_FLOAT), GLboolean(GL_FALSE), GLsizei(ParticleVertex.size), BUFFER_OFFSET(0))
-
-        glEnableVertexAttribArray(GLuint(GLKVertexAttrib.Color.rawValue))
-        glVertexAttribPointer(GLuint(GLKVertexAttrib.Color.rawValue), 4, GLenum(GL_FLOAT), GLboolean(GL_FALSE), GLsizei(ParticleVertex.size), BUFFER_OFFSET(sizeof(Float) * 3))
-
-        glEnableVertexAttribArray(ParticleShaderProgram.PointSizeAttribLocation)
-        glVertexAttribPointer(ParticleShaderProgram.PointSizeAttribLocation, 1, GLenum(GL_FLOAT), GLboolean(GL_FALSE), GLsizei(ParticleVertex.size), BUFFER_OFFSET(sizeof(Float) * 7))
-
-        glDrawArrays(GLenum(GL_POINTS), 0, GLsizei(points.count))
-
-        glBindVertexArray(0)
     }
 
     private func renderBackground() {
@@ -265,7 +116,7 @@ class GameSceneRenderer {
         glBindTexture(GLenum(GL_TEXTURE_2D), whiteTextureInfo.name)
         drawModels(modelProducer.modelObjects())
 
-        glBindTexture(GLenum(GL_TEXTURE_2D), fontData.textureInfo.name)
+        glBindTexture(GLenum(GL_TEXTURE_2D), FontData.defaultData.textureInfo.name)
         drawModels(modelProducer.labelObjects().map { $0 as Renderable})
     }
 
@@ -316,7 +167,7 @@ class GameSceneRenderer {
         glBindTexture(GLenum(GL_TEXTURE_2D), whiteTextureInfo.name)
         drawModels(modelProducer.uiObjects())
 
-        glBindTexture(GLenum(GL_TEXTURE_2D), fontData.textureInfo.name)
+        glBindTexture(GLenum(GL_TEXTURE_2D), FontData.defaultData.textureInfo.name)
         drawModels(modelProducer.uiLabelObjects())
     }
 
