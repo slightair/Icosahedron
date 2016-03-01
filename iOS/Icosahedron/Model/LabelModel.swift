8bbded27b84239dcc25fb6e6df134788cef8e032
slightair <arksutite@gmail.com>
2016-03-01 23:14:46 +0900

    Change label background color

--- iOS/Icosahedron/Model/LabelModel.swift
+++ iOS/Icosahedron/Model/LabelModel.swift
@@ -2,7 +2,7 @@ import GLKit
 import RxSwift
 
 class LabelModel: Renderable {
-    static let defaultBackgroundColor = UIColor.flatWhiteColorDark().glColor
+    static let defaultBackgroundColor = UIColor.flatBlackColor().colorWithAlphaComponent(0.2).glColor
 
     var position = GLKVector3Make(0.0, 0.0, 0.0)
     var quaternion = GLKQuaternionIdentity
