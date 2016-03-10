9c9bb6c5e63bfde2549bd25a2334f8ebd24f02e1
slightair <arksutite@gmail.com>
2016-03-10 13:28:30 +0900

    Face detect from tracks (WIP)

--- iOS/Icosahedron/Entity/SymbolDetector.swift
+++ iOS/Icosahedron/Entity/SymbolDetector.swift
@@ -1,7 +1,22 @@
 import Foundation
 
 class SymbolDetector {
+    static let faceMaterials: [Set<Icosahedron.Side>: Icosahedron.Face] = [
+        [.AB, .BC, .AC]: .ACB,
+    ]
+
     static func facesFromTracks(tracks: [Track]) -> Set<Icosahedron.Face> {
-        return []
+        let sides = Set<Icosahedron.Side>(tracks.map { $0.side })
+        var faces: Set<Icosahedron.Face> = []
+
+        for (materials, face) in SymbolDetector.faceMaterials {
+            let targetSides = sides.intersect(materials)
+
+            if targetSides.count == 3 {
+                faces.insert(face)
+            }
+        }
+
+        return faces
     }
 }
