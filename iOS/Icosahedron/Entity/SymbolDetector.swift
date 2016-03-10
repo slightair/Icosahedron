998dc1d61500bcc2f4928deb1ef0817d9ef22b6d
slightair <arksutite@gmail.com>
2016-03-10 19:54:05 +0900

    Add faceMaterials

--- iOS/Icosahedron/Entity/SymbolDetector.swift
+++ iOS/Icosahedron/Entity/SymbolDetector.swift
@@ -2,7 +2,26 @@ import Foundation
 
 class SymbolDetector {
     static let faceMaterials: [Set<Icosahedron.Side>: Icosahedron.Face] = [
-        [.AB, .BC, .AC]: .ACB,
+        [.AB, .AF, .BF]: .ABF,
+        [.AB, .AC, .BC]: .ACB,
+        [.AC, .AE, .CE]: .AEC,
+        [.AF, .AG, .FG]: .AFG,
+        [.AE, .AG, .GE]: .AGE,
+        [.BC, .BD, .CD]: .BCD,
+        [.BD, .BH, .DH]: .BDH,
+        [.BF, .BH, .FH]: .BHF,
+        [.CE, .CI, .EI]: .CEI,
+        [.CD, .CI, .DI]: .CID,
+        [.DI, .DJ, .IJ]: .DIJ,
+        [.DH, .DJ, .HJ]: .DJH,
+        [.EK, .GE, .GK]: .EGK,
+        [.EI, .EK, .IK]: .EKI,
+        [.FH, .FL, .HL]: .FHL,
+        [.FG, .FL, .GL]: .FLG,
+        [.GK, .GL, .KL]: .GLK,
+        [.HJ, .HL, .JL]: .HJL,
+        [.IJ, .IK, .JK]: .IKJ,
+        [.JK, .JL, .KL]: .JKL,
     ]
 
     static func facesFromTracks(tracks: [Track]) -> Set<Icosahedron.Face> {
