2d32645aa3efd09839447ddeec0aa88bee64caa7
slightair <arksutite@gmail.com>
2016-03-25 00:14:11 +0900

    Support rhombus symbol detect

--- iOS/Icosahedron/Entity/SymbolDetector.swift
+++ iOS/Icosahedron/Entity/SymbolDetector.swift
@@ -14,33 +14,33 @@ class SymbolDetector {
         }
     }
 
-    static let faceMaterials: [Set<Icosahedron.Side>: Icosahedron.Face] = [
-        [.AB, .AF, .BF]: .ABF,
-        [.AB, .AC, .BC]: .ACB,
-        [.AC, .AE, .CE]: .AEC,
-        [.AF, .AG, .FG]: .AFG,
-        [.AE, .AG, .GE]: .AGE,
-        [.BC, .BD, .CD]: .BCD,
-        [.BD, .BH, .DH]: .BDH,
-        [.BF, .BH, .FH]: .BHF,
-        [.CE, .CI, .EI]: .CEI,
-        [.CD, .CI, .DI]: .CID,
-        [.DI, .DJ, .IJ]: .DIJ,
-        [.DH, .DJ, .HJ]: .DJH,
-        [.EK, .GE, .GK]: .EGK,
-        [.EI, .EK, .IK]: .EKI,
-        [.FH, .FL, .HL]: .FHL,
-        [.FG, .FL, .GL]: .FLG,
-        [.GK, .GL, .KL]: .GLK,
-        [.HJ, .HL, .JL]: .HJL,
-        [.IJ, .IK, .JK]: .IKJ,
-        [.JK, .JL, .KL]: .JKL,
+    static let faceMaterials: [Icosahedron.Face: Set<Icosahedron.Side>] = [
+        .ABF: [.AB, .AF, .BF],
+        .ACB: [.AB, .AC, .BC],
+        .AEC: [.AC, .AE, .CE],
+        .AFG: [.AF, .AG, .FG],
+        .AGE: [.AE, .AG, .GE],
+        .BCD: [.BC, .BD, .CD],
+        .BDH: [.BD, .BH, .DH],
+        .BHF: [.BF, .BH, .FH],
+        .CEI: [.CE, .CI, .EI],
+        .CID: [.CD, .CI, .DI],
+        .DIJ: [.DI, .DJ, .IJ],
+        .DJH: [.DH, .DJ, .HJ],
+        .EGK: [.EK, .GE, .GK],
+        .EKI: [.EI, .EK, .IK],
+        .FHL: [.FH, .FL, .HL],
+        .FLG: [.FG, .FL, .GL],
+        .GLK: [.GK, .GL, .KL],
+        .HJL: [.HJ, .HL, .JL],
+        .IKJ: [.IJ, .IK, .JK],
+        .JKL: [.JK, .JL, .KL],
     ]
 
     static func facesFromTracks(tracks: [Track]) -> Set<Face> {
         var faces: Set<Face> = []
 
-        for (materials, face) in SymbolDetector.faceMaterials {
+        for (face, materials) in SymbolDetector.faceMaterials {
             let targetTracks = tracks.filter { materials.contains($0.side) }
             let targetColors = Set<World.Color>(targetTracks.map { $0.color })
 
@@ -54,12 +54,15 @@ class SymbolDetector {
 
     static func symbolsFromTracks(tracks: [Track]) -> Set<Symbol> {
         var symbols: Set<Symbol> = []
+        var faces: Set<Face> = []
 
-        for (materials, _) in SymbolDetector.faceMaterials {
+        for (face, materials) in SymbolDetector.faceMaterials {
             let targetTracks = tracks.filter { materials.contains($0.side) }
             let targetColors = Set<World.Color>(targetTracks.map { $0.color })
 
             if targetTracks.count == 3 && targetColors.count == 1 {
+                faces.insert(Face(face: face, color: targetColors.first!))
+
                 let triangle: Symbol
                 switch targetColors.first! {
                 case .Red:
@@ -70,6 +73,31 @@ class SymbolDetector {
                     triangle = .BlueTriangle
                 }
                 symbols.insert(triangle)
+            }
+        }
+
+        func rhombusForColor(color: World.Color) -> Symbol {
+            switch color {
+            case .Red:
+                return .RedRhombus
+            case .Green:
+                return .GreenRhombus
+            case .Blue:
+                return .BlueRhombus
+            }
+        }
+
+        for color in World.Color.values {
+            let triangles = faces.filter { $0.color == color }
+            for triangle in triangles {
+                for other in (triangles.filter { $0 != triangle }) {
+                    let triangleSides = faceMaterials[triangle.face]!
+                    let otherSides = faceMaterials[other.face]!
+                    let sides = triangleSides.union(otherSides)
+                    if sides.count == 5 {
+                        symbols.insert(rhombusForColor(color))
+                    }
+                }
             }
         }
 
--- iOS/IcosahedronTests/SymbolDetectorTests.swift
+++ iOS/IcosahedronTests/SymbolDetectorTests.swift
@@ -95,6 +95,22 @@ class SymbolDetectorTests: XCTestCase {
         ])
     }
 
+    func testFacesFromTracksHasTwoFacesRhombus2() {
+        let tracks: [Track] = [
+            Track(start: .C, end: .I, color: .Green, turn:1),
+            Track(start: .E, end: .K, color: .Green, turn:3),
+            Track(start: .K, end: .I, color: .Green, turn:4),
+            Track(start: .I, end: .E, color: .Green, turn:5),
+            Track(start: .E, end: .C, color: .Green, turn:6),
+        ]
+        let faces = SymbolDetector.facesFromTracks(tracks)
+
+        XCTAssertEqual(faces, [
+            SymbolDetector.Face(face: .CEI, color: .Green),
+            SymbolDetector.Face(face: .EKI, color: .Green),
+        ])
+    }
+
     func testFacesFromTracksHasFourFacesSuperTriangle() {
         let tracks: [Track] = [
             Track(start: .C, end: .B, color: .Red, turn:1),
@@ -207,9 +223,7 @@ class SymbolDetectorTests: XCTestCase {
             Track(start: .C, end: .B, color: .Red, turn:3),
             Track(start: .B, end: .D, color: .Red, turn:4),
             Track(start: .D, end: .C, color: .Red, turn:5),
-
             Track(start: .C, end: .I, color: .Green, turn:6),
-            Track(start: .I, end: .E, color: .Green, turn:7),
             Track(start: .E, end: .K, color: .Green, turn:8),
             Track(start: .K, end: .I, color: .Green, turn:9),
             Track(start: .I, end: .E, color: .Green, turn:10),
