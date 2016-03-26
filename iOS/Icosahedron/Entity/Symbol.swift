b9f5138da1bfb46597985435fee1eceb068333a9
slightair <arksutite@gmail.com>
2016-03-26 22:42:29 +0900

    Remove unnecessary symbols

--- iOS/Icosahedron/Entity/Symbol.swift
+++ iOS/Icosahedron/Entity/Symbol.swift
@@ -4,25 +4,11 @@ enum Symbol {
     case RedTriangle
     case GreenTriangle
     case BlueTriangle
-
     case RedRhombus
     case GreenRhombus
     case BlueRhombus
-
     case RedSuperTriangle
     case GreenSuperTriangle
     case BlueSuperTriangle
-
-    case RedGreenRhombus
-    case RedBlueRhombus
-    case GreenBlueRhombus
-
-    case RedGreenSuperTriangle
-    case RedBlueSuperTriangle
-    case GreenRedSuperTriangle
-    case GreenBlueSuperTriangle
-    case BlueRedSuperTriangle
-    case BlueGreenSuperTriangle
-
     case FullColorSuperTriangle
 }
