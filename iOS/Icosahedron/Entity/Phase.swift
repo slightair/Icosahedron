cdf7708487ad59bffe68266dca56c15f245eeb80
slightair <arksutite@gmail.com>
2016-03-31 22:57:12 +0900

    Make problem pattern

--- iOS/Icosahedron/Entity/Phase.swift
+++ iOS/Icosahedron/Entity/Phase.swift
@@ -1,6 +1,10 @@
 import Foundation
+import GameplayKit
 
 class Phase {
+    static let costs = [0, 1, 2, 3, 5, 7, 10, 12, 15]
+    static let randomSource = GKMersenneTwisterRandomSource(seed: 3962)
+
     let number: Int
     let problems: [Symbol]
 
@@ -10,9 +14,24 @@ class Phase {
     }
 
     static func problemsForPhaseNumber(number: Int) -> [Symbol] {
-        if number > Symbol.values.count {
-            return Symbol.values
+        var cost: Int
+        if number >= costs.count {
+            cost = Phase.costs.last!
+        } else {
+            cost = Phase.costs[number]
         }
-        return Array(Symbol.values.prefix(number))
+
+        var problems = [Symbol]()
+        while cost > 0 {
+            let candidates = Symbol.costs.filter { $0.1 <= cost }.map { ($0.0, $0.1) }
+
+            let needle = randomSource.nextIntWithUpperBound(candidates.count)
+            let (symbol, symbolCost) = candidates[needle]
+
+            problems.append(symbol)
+            cost -= symbolCost
+        }
+
+        return problems
     }
 }
--- iOS/Icosahedron/Entity/Symbol.swift
+++ iOS/Icosahedron/Entity/Symbol.swift
@@ -24,4 +24,17 @@ enum Symbol {
         .BlueSuperTriangle,
         .FullColorSuperTriangle,
     ]
+
+    static let costs: [Symbol: Int] = [
+        .RedTriangle: 1,
+        .GreenTriangle: 1,
+        .BlueTriangle: 1,
+        .RedRhombus: 5,
+        .GreenRhombus: 5,
+        .BlueRhombus: 5,
+        .RedSuperTriangle: 15,
+        .GreenSuperTriangle: 15,
+        .BlueSuperTriangle: 15,
+        .FullColorSuperTriangle: 50,
+    ]
 }
