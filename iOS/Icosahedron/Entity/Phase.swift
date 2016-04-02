7e0ad427f2f0f75cf554eee72fc01594288e61b5
slightair <arksutite@gmail.com>
2016-04-02 20:57:22 +0900

    Change cost calculation

--- iOS/Icosahedron/Entity/Phase.swift
+++ iOS/Icosahedron/Entity/Phase.swift
@@ -2,7 +2,6 @@ import Foundation
 import GameplayKit
 
 class Phase {
-    static let costs = [0, 1, 2, 3, 5, 7, 10, 12, 15]
     static let randomSource = GKMersenneTwisterRandomSource(seed: 3962)
 
     let number: Int
@@ -14,13 +13,7 @@ class Phase {
     }
 
     static func problemsForPhaseNumber(number: Int) -> [Symbol] {
-        var cost: Int
-        if number >= costs.count {
-            cost = Phase.costs.last!
-        } else {
-            cost = Phase.costs[number]
-        }
-
+        var cost = number
         var problems = [Symbol]()
         while cost > 0 {
             let candidates = Symbol.costs.filter { $0.1 <= cost }.map { ($0.0, $0.1) }
